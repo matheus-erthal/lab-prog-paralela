9cb1411092e53d38f7043a42b755d0ba0e4be10b
Matheus Erthal <matheusamancio@id.uff.br>
2019-09-03 09:07:14 -0300

    Multiplicação paralelizada

--- aula3/mult_matriz.c
+++ aula3/mult_matriz.c
@@ -4,27 +4,40 @@
 
 int main(){
 
-	int N = 1000;
+	omp_set_num_threads(4);
+
+	int N = 500;
 
 	int i, j, k;
 
 	int matriz_a[N][N], matriz_b[N][N], matriz_c[N][N];
 
-	for(i = 0; i < N; i++){
-		for(j = 0; j < N; j++){
-			matriz_a[i][j] = 1;
-			matriz_b[i][j] = 1;
+	#pragma omp parallel
+	{	
+		#pragma omp for
+		for(i = 0; i < N; i++){
+			for(j = 0; j < N; j++){
+				matriz_a[i][j] = 1;
+				matriz_b[i][j] = 1;
+				matriz_c[i][j] = 0;
+			}
 		}
 	}
 
-	for(i = 0; i < N; i++){
-		for(j = 0; j < N; j++){
-			matriz_c[i][j] = 0;
-			for(k = 0; k < N; k++){
-				matriz_c[i][j] = matriz_c[i][j] + matriz_a[i][j] * matriz_b[i][j];
+	#pragma omp parallel private(i, j, k)
+	{	
+		#pragma omp for
+		for(i = 0; i < N; i++){
+			for(j = 0; j < N; j++){
+				for(k = 0; k < N; k++){
+					matriz_c[i][j] = matriz_c[i][j] + matriz_a[i][j] * matriz_b[i][j];
+				}
 			}
-			printf("%d\n", matriz_c[i][j]);
 		}
 	}
-	
+	for(i = 0; i < N; i++){
+		for(j = 0; j < N; j++){
+			printf("%d\n", matriz_c[i][j]);
+		}	
+	}
 }
